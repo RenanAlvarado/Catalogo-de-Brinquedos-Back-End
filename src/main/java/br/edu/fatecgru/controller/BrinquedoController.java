8083704a72f123e681a4062c49669edd6311b36c
Renan Amaral <renanalvarado123@gmail.com>
2026-03-08 00:17:03 -0300

    CrossOrigin inserido para utilização do Live Server

--- src/main/java/br/edu/fatecgru/controller/BrinquedoController.java
+++ src/main/java/br/edu/fatecgru/controller/BrinquedoController.java
@@ -3,6 +3,7 @@ package br.edu.fatecgru.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.edu.fatecgru.model.entity.Brinquedo;
 import br.edu.fatecgru.service.BrinquedoService;
 
+@CrossOrigin(origins = "*")
 @RestController
 //Cria o geral, todos precisam desse
 @RequestMapping("/api/brinquedos")
