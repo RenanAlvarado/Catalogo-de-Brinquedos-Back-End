bce2c377a97cca4b2a750c854d598d90b959256b
math-fsilva <matheusmf755887@gmail.com>
2026-03-08 21:23:45 -0300

    Criação do método de busca por categoria

--- src/main/java/br/edu/fatecgru/controller/CategoriaController.java
+++ src/main/java/br/edu/fatecgru/controller/CategoriaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,11 @@ public class CategoriaController {
 	public List<Categoria> listarTodos() {
 		return categoriaService.listarTodos();
 	}
+	
+	// Busca de categoria pelo nome
+	@GetMapping("/{nome}")
+		public List<Categoria> buscarPorContemNome(@PathVariable String nome) {
+			return categoriaService.getByContainsName(nome);
+		}
 
 }
--- src/main/java/br/edu/fatecgru/repository/CategoriaRepository.java
+++ src/main/java/br/edu/fatecgru/repository/CategoriaRepository.java
@@ -1,9 +1,14 @@
 package br.edu.fatecgru.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.fatecgru.model.entity.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
+	// Método para fazer buscar por categoria
+	public List<Categoria> findByNomeContains(String nome);
+	
 }
