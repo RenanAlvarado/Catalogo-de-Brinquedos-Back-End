e31c62baae30b5f909cba35aad20321ff2fba957
Renan Amaral <renanalvarado123@gmail.com>
2026-03-10 10:34:04 -0300

    Alterações minhas

--- src/main/java/br/edu/fatecgru/model/entity/TipoUsuario.java
+++ src/main/java/br/edu/fatecgru/model/entity/TipoUsuario.java
@@ -2,6 +2,5 @@ package br.edu.fatecgru.model.entity;
 
 // Enum para determinar o tipo do usuário com administrador ou cliente
 public enum TipoUsuario {
-	    ADMIN,
-	    CLIENTE
-	}
+	ADMIN, CLIENTE
+}
