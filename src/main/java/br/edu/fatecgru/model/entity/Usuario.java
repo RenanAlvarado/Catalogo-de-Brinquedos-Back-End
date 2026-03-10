a78a31ec23cddd22ca077bdc165aa69ec9c91709
math-fsilva <matheusmf755887@gmail.com>
2026-03-10 06:33:21 -0300

    Atributos, construtores, getters/setters da classe usuário

--- src/main/java/br/edu/fatecgru/model/entity/Usuario.java
+++ src/main/java/br/edu/fatecgru/model/entity/Usuario.java
@@ -1,5 +1,85 @@
 package br.edu.fatecgru.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 
+	// Atributos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String nome, email, senha, imagem, tipo;
+	
+	// Construtores
+	public Usuario() {
+		
+	}
+
+	public Usuario(int id, String nome, String email, String senha, String imagem, String tipo) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.imagem = imagem;
+		this.tipo = tipo;
+	}
+	
+	// Getter e setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
 }
