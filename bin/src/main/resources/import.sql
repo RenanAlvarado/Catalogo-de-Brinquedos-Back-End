816e2e1920281d77d6334f58769128a798d7dad5
math-fsilva <matheusmf755887@gmail.com>
2026-03-11 03:38:35 -0300

    Correção no import.sql

---
+++ bin/src/main/resources/import.sql
@@ -0,0 +1,31 @@
+INSERT INTO brinquedos (nome, descricao, imagem, preco) VALUES ('Carrinho Hot Wheels', 'Carrinho de metal em miniatura da linha Hot Wheels', 'hotwheels.jpg', 19.90);
+
+INSERT INTO brinquedos (nome, descricao, imagem, preco) VALUES ('Boneca Barbie', 'Boneca Barbie classica com acessorios', 'barbie.jpg', 89.90);
+
+INSERT INTO brinquedos (nome, descricao, imagem, preco) VALUES ('Lego City Policia', 'Conjunto Lego City com viatura policial', 'lego-police.jpg', 159.90);
+
+INSERT INTO brinquedos (nome, descricao, imagem, preco) VALUES ('Dinossauro T-Rex', 'Dinossauro de brinquedo articulado', 'trex.jpg', 49.90);
+
+INSERT INTO brinquedos (nome, descricao, imagem, preco) VALUES ('Quebra cabeca 500 pecas', 'Quebra cabeca infantil de 500 pecas', 'puzzle500.jpg', 39.90);
+
+INSERT INTO brinquedos (nome, descricao, imagem, preco) VALUES ('Caminhao de Bombeiro', 'Caminhao de bombeiro com escada movel', 'bombeiro.jpg', 74.90);
+
+INSERT INTO brinquedos (nome, descricao, imagem, preco) VALUES ('Urso de Pelucia', 'Urso de pelucia macio tamanho medio', 'urso.jpg', 59.90);
+
+INSERT INTO brinquedos (nome, descricao, imagem, preco) VALUES ('Pista Hot Wheels', 'Pista de corrida Hot Wheels com loop', 'pista.jpg', 129.90);
+
+INSERT INTO categorias (nome, imagem) VALUES ('Carrinhos', 'carrinhos.jpg');
+
+INSERT INTO categorias (nome, imagem) VALUES ('Bonecas', 'bonecas.jpg');
+
+INSERT INTO categorias (nome, imagem) VALUES ('Educativos', 'educativos.jpg');
+
+INSERT INTO categorias (nome, imagem) VALUES ('Jogos de Tabuleiro', 'jogos-tabuleiro.jpg');
+
+INSERT INTO categorias (nome, imagem) VALUES ('Pelucias', 'pelucias.jpg');
+
+INSERT INTO categorias (nome, imagem) VALUES ('Blocos de Montar', 'blocos-montar.jpg');
+
+INSERT INTO categorias (nome, imagem) VALUES ('Brinquedos para Bebe', 'bebe.jpg');
+
+INSERT INTO categorias (nome, imagem) VALUES ('Brinquedos Eletronicos', 'eletronicos.jpg');
