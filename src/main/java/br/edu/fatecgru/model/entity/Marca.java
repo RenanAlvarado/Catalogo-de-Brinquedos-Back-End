4787747185bde5bf6726268bd0a0a5f61d1df1ec
math-fsilva <matheusmf755887@gmail.com>
2026-03-17 12:10:42 -0300

    Correção nos métodos

--- src/main/java/br/edu/fatecgru/model/entity/Marca.java
+++ src/main/java/br/edu/fatecgru/model/entity/Marca.java
@@ -15,19 +15,14 @@ public class Marca {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	public String nome, imagem;
+	private String nome, imagem;
 
 	// Construtores
 	public Marca() {
 
 	}
 
-	public Marca(String nome, String imagem) {
-
-	}
-
 	public Marca(Integer id, String nome, String imagem) {
-
 		this.id = id;
 		this.nome = nome;
 		this.imagem = imagem;
